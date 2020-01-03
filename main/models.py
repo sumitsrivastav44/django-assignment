9796dba90e742bf4f61b06333ff8d848c019647b
sumitsrivastav <sumitsrivastav44@gmail.com>
2020-01-03 20:09:55 +0530

    text field changed to about field

--- main/models.py
+++ main/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from datetime import datetime
 
 class Document(models.Model):
-	about = models.TextField()
+	about = models.CharField(max_length=120)
 	file = models.FileField(upload_to='user_docs')
 	date_posted = models.CharField(max_length=70 ,default=datetime.now().strftime("%d %B %Y %I:%M%p"))
